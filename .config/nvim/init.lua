ee49a629848c983c0b5dd6fe9436469c94c3fee9
Carl Smedstad <carl.smedstad@protonmail.com>
2022-01-24 21:09:49 +0100

    nvim: Add more fenced languages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -149,7 +149,7 @@ require("packer").startup(function()
 end)
 
 vim.g.mapleader = ","
-vim.g.markdown_fenced_languages = { "json", "yaml", "sh", "bash=sh", "python", "ini=dosini" }
+vim.g.markdown_fenced_languages = { "json", "yaml", "sh", "bash=sh", "python", "ini=dosini", "vim", "lua" }
 
 if vim.env.COLORTERM == "truecolor" then
   vim.opt.termguicolors = true
