345b9c58261bbc1967257709e4a0db6c77cfe4e6
Carl Smedstad <carl.smedstad@protonmail.com>
2022-05-18 15:47:51 +0200

    system: Don't set Wayland envs - breaks X11 apps

--- system/usr/local/lib/wayland-enablement.sh
+++ system/usr/local/lib/wayland-enablement.sh
@@ -1,9 +1,6 @@
 # shellcheck shell=sh
 
 export MOZ_ENABLE_WAYLAND=1
-export GDK_BACKEND=wayland
-export CLUTTER_BACKEND=wayland
-export QT_QPA_PLATFORM=wayland-egl
 export ECORE_EVAS_ENGINE=wayland-egl
 export ELM_ENGINE=wayland_egl
 export SDL_VIDEODRIVER=wayland
