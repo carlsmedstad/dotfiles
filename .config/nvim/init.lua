15b57ef00ef0d9d55db71356c21ec53e3a14ba15
Carl Smedstad <carl.smedstad@protonmail.com>
2023-07-21 14:19:33 +0200

    nvim: Add treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,76 @@ end
 require("packer").startup(function()
   -- luacheck: push globals use
   use({
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+      ts_update()
+    end,
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "awk",
+          "bash",
+          "bicep",
+          "c",
+          "cmake",
+          "comment",
+          "cpp",
+          "diff",
+          "dockerfile",
+          "dot",
+          "git_config",
+          "git_rebase",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
+          "go",
+          "html",
+          "http",
+          "hurl",
+          "ini",
+          "javascript",
+          "jq",
+          "json",
+          "lua",
+          "luadoc",
+          "make",
+          "markdown",
+          "markdown_inline",
+          "meson",
+          "python",
+          "regex",
+          "rst",
+          "sql",
+          "toml",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "vue",
+          "yaml",
+        },
+        highlight = { enable = true, additional_vim_regex_highlighting = false },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+        indent = { enable = true },
+      })
+      vim.keymap.set(
+        "n",
+        "<leader>t",
+        ":lua print(require('nvim-treesitter.ts_utils').get_node_at_cursor())<CR>",
+        { silent = true }
+      )
+    end,
+  })
+
+  use({
     "wbthomason/packer.nvim",
     config = function()
       vim.cmd([[
