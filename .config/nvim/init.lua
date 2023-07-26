b68cb887aaaf0eda8129b72d59c3bf56aa0a42e5
Carl Smedstad <carl.smedstad@protonmail.com>
2023-07-26 19:24:44 +0200

    nvim: Exclude shellcheck warning 2128 for PKGBUILD files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -377,7 +377,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "PKGBUILD",
   callback = function()
     vim.g.ale_sh_shellcheck_dialect = "bash"
-    vim.g.ale_sh_shellcheck_exclusions = "2164,2154,2034"
+    vim.g.ale_sh_shellcheck_exclusions = "2034,2128,2154,2164"
   end,
 })
 
