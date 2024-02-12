4798ca5ae1418e2aabc69c58ea1d198e8e81de9d
Carl Smedstad <carl.smedstad@protonmail.com>
2024-02-12 08:52:48 +0100

    fish: Move fish configs to ~/.config/fish/config.d/

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
-    source $XDG_CONFIG_HOME/fish/aliases.fish
-    source $XDG_CONFIG_HOME/fish/gpg-agent.fish
+    for file in $XDG_CONFIG_HOME/fish/config.d/*
+      source $file
+    end
     set -U fish_color_cwd f9e2af
     set -g fish_sheme "Catppuccin Mocha"
     set -g fish_greeting ""
