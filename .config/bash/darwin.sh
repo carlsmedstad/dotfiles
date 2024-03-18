8a7336e96a0bf0035ead00a6f37339b7b78a44da
Carl Smedstad <carl.smedstad@protonmail.com>
2024-03-18 16:15:44 +0100

    bash: Fix coreutils on MacOS and refactor init file

--- .config/bash/darwin.sh
+++ .config/bash/darwin.sh
@@ -1,14 +1,20 @@
-# shellcheck shell=bash disable=1091
+# shellcheck shell=bash disable=1090,1091
 
-if [ "$(uname)" = "Darwin" ]; then
-  [ -f /opt/homebrew/bin/brew ] \
-    && eval "$(/opt/homebrew/bin/brew shellenv)"
-  [ -f /opt/homebrew/etc/profile.d/bash_completion.sh ] \
-    && . /opt/homebrew/etc/profile.d/bash_completion.sh
-  [ -f "$HOME/.fzf.bash" ] \
-    && . "$HOME/.fzf.bash"
-  [ -f /usr/local/opt/fzf/shell/key-bindings.bash ] \
-    && . /usr/local/opt/fzf/shell/key-bindings.bash
-  [ -f /usr/local/opt/fzf/shell/completion.bash ] \
-    && . /usr/local/opt/fzf/shell/completion.bash
-fi
+[ "$(uname)" = "Darwin" ] || return
+
+
+[ -f /opt/homebrew/bin/brew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
+
+bash_completion_file="/opt/homebrew/etc/profile.d/bash_completion.sh"
+[ -f "$bash_completion_file" ] && . "$bash_completion_file"
+
+coreutils_bindir="/opt/homebrew/opt/coreutils/libexec/gnubin"
+[ -d "$coreutils_bindir" ] && export PATH="$coreutils_bindir:$PATH"
+
+fzf_key_bindings_file="/opt/homebrew/opt/fzf/shell/key-bindings.bash"
+[ -f "$fzf_key_bindings_file" ] && . "$fzf_key_bindings_file"
+
+fzf_completion_file="/opt/homebrew/opt/fzf/shell/completion.bash"
+[ -f "$fzf_completion_file" ] && . "$fzf_completion_file"
+
+[ -f "$HOME/.fzf.bash" ] && . "$HOME/.fzf.bash"
