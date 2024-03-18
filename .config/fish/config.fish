4b2c985f70403a53d516623d80fdec44e024e926
Carl Smedstad <carl.smedstad@protonmail.com>
2024-03-18 16:34:02 +0100

    fish: Move .config/fish/{config.d -> conf.d}

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,4 @@
 if status is-interactive
-    for file in $XDG_CONFIG_HOME/fish/config.d/*
-      source $file
-    end
     set -U fish_color_cwd f9e2af
     set -g fish_sheme "Catppuccin Mocha"
     set -g fish_greeting ""
