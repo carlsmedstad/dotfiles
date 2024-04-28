29084ff753b8f005b66ce3fc228e6de80cc1867d
Carl Smedstad <carl.smedstad@protonmail.com>
2024-04-28 22:50:23 +0200

    fish: Simplify gpg-agent config

--- .config/fish/conf.d/gpg-agent.fish
+++ .config/fish/conf.d/gpg-agent.fish
@@ -1,5 +1,3 @@
 set -x GPG_TTY (tty)
 set -e SSH_AGENT_PID
-if test "$gnupg_SSH_AUTH_SOCK_by" != $fish_pid
-    set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-end
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
