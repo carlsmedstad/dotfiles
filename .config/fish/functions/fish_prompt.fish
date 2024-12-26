81f67fe4438d6f7db1405b43b7a184eafe61d745
Carl Smedstad <carl.smedstad@protonmail.com>
2024-12-26 16:10:28 +0100

    fish: Remove special prompt for root

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -5,16 +5,6 @@ function fish_prompt --description 'Write out the prompt'
     set -q fish_color_status
     or set -g fish_color_status red
 
-    # Color the prompt differently when we're root
-    set -l color_cwd $fish_color_cwd
-    set -l suffix '$'
-    if functions -q fish_is_root_user; and fish_is_root_user
-        if set -q fish_color_cwd_root
-            set color_cwd $fish_color_cwd_root
-        end
-        set suffix '#'
-    end
-
     # Write pipestatus
     # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
     set -l bold_flag --bold
@@ -27,6 +17,9 @@ function fish_prompt --description 'Write out the prompt'
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
     set -l current_time (date "+%H:%M:%S")
+
+    set -l color_cwd $fish_color_cwd
+    set -l suffix '$'
 
     echo -n -s (set_color "#fab387") $current_time " " (set_color $color_cwd) (prompt_pwd) $normal \
         (fish_vcs_prompt) $normal " "$prompt_status " " $suffix " "
