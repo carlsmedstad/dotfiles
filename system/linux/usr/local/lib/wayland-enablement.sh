0cfde1c462d760009a087a734f58e388f3214de8
Carl Smedstad <carl.smedstad@protonmail.com>
2025-03-02 18:07:48 +0200

    Remove bootstrap, pkgs & system - handled by kits

--- system/linux/usr/local/lib/wayland-enablement.sh
+++
@@ -1,8 +0,0 @@
-# shellcheck shell=sh
-
-export MOZ_ENABLE_WAYLAND=1
-export ECORE_EVAS_ENGINE=wayland-egl
-export ELM_ENGINE=wayland_egl
-export SDL_VIDEODRIVER=wayland
-export _JAVA_AWT_WM_NONREPARENTING=1
-export NO_AT_BRIDGE=1
