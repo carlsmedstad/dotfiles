cad796a197f209f9e78156ad5f46c01653d0796d
Carl Smedstad <carl.smedstad@protonmail.com>
2026-04-06 01:22:47 +0200

    nvim: Fix nvim-treesitter 0.12 compatibility, switch to main branch

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,72 +1,75 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "awk",
-          "bash",
-          "bicep",
-          "c",
-          "cmake",
-          "comment",
-          "cpp",
-          "diff",
-          "dockerfile",
-          "dot",
-          "git_config",
-          "git_rebase",
-          "gitattributes",
-          "gitcommit",
-          "gitignore",
-          "go",
-          "gomod",
-          "gosum",
-          "helm",
-          "html",
-          "http",
-          "hurl",
-          "ini",
-          "javascript",
-          "jq",
-          "json",
-          "jsonnet",
-          "lua",
-          "luadoc",
-          "make",
-          "markdown",
-          "markdown_inline",
-          "meson",
-          "python",
-          "regex",
-          "rst",
-          "sql",
-          "toml",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "vue",
-          "yaml",
-        },
-        highlight = { enable = true, additional_vim_regex_highlighting = false },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-          },
-        },
-        indent = { enable = true },
+      require("nvim-treesitter").setup({})
+
+      -- Install parsers (replaces ensure_installed)
+      local installed = require("nvim-treesitter.config").get_installed()
+      local wanted = {
+        "awk",
+        "bash",
+        "bicep",
+        "c",
+        "cmake",
+        "comment",
+        "cpp",
+        "diff",
+        "dockerfile",
+        "dot",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "gomod",
+        "gosum",
+        "helm",
+        "html",
+        "http",
+        "hurl",
+        "ini",
+        "javascript",
+        "jq",
+        "json",
+        "jsonnet",
+        "lua",
+        "luadoc",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "meson",
+        "python",
+        "regex",
+        "rst",
+        "sql",
+        "toml",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "vue",
+        "yaml",
+      }
+      local to_install = vim.iter(wanted)
+        :filter(function(p)
+          return not vim.tbl_contains(installed, p)
+        end)
+        :totable()
+      if #to_install > 0 then
+        require("nvim-treesitter").install(to_install)
+      end
+    end,
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
-      vim.keymap.set(
-        "n",
-        "<leader>t",
-        ":lua print(require('nvim-treesitter.ts_utils').get_node_at_cursor())<CR>",
-        { silent = true }
-      )
     end,
   },
 }
